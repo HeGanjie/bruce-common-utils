59c77da51709e73d55ce9ec4e124c0629edf0545
heganjie <heganjie@gmail.com>
2014-05-12 00:46:43 +0800

    append PersistentVector to lambdaUtils

---
+++ src/bruce/common/functional/PersistentCollection.java
@@ -0,0 +1,34 @@
+package bruce.common.functional;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Iterator;
+
+public interface PersistentCollection<E> extends Serializable {
+	public Collection<E> getCollection();
+
+	public PersistentCollection<E> conj(E e);
+
+	public PersistentCollection<E> merge(PersistentCollection<? extends E> c);
+
+	public boolean contains(E o);
+
+	public boolean containsAll(PersistentCollection<? extends E> c);
+
+	public boolean isEmpty();
+
+	public Iterator<E> iterator();
+
+	public PersistentCollection<E> disj(E o);
+
+	public PersistentCollection<E> disjAll(PersistentCollection<? extends E> c);
+
+	public PersistentCollection<E> retainAll(PersistentCollection<? extends E> c);
+
+	public int size();
+
+	public Object[] toArray();
+
+	public <T> T[] toArray(T[] a);
+
+}
