fdc77325fbfc8d948a9c203ac0aec28148389f3a
heganjie <heganjie@gmail.com>
2014-08-28 13:37:14 +0800

    make pointer compareable

--- src/bruce/common/functional/Pointer.java
+++ src/bruce/common/functional/Pointer.java
@@ -7,4 +7,24 @@ public final class Pointer<T> {
 	}
 
 	public T value;
+
+	@Override
+	public String toString() { return "Pointer [value=" + value + "]"; }
+	
+	@Override
+	public int hashCode() { return 31 + ((value == null) ? 0 : value.hashCode()); }
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		Pointer other = (Pointer) obj;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
 }
